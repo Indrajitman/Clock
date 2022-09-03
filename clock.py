222da0e3b1d1d113c9ab61bdd7d2bba539411292
Indrajitman <112776077+Indrajitman@users.noreply.github.com>
2022-09-03 23:40:48 +0530

    Add files via upload

---
+++ clock.py
@@ -0,0 +1,18 @@
+'This code is to make a clock'
+from tkinter import*
+from tkinter.ttk import*
+
+from time import strftime
+root=Tk()
+root.title("Clock")
+
+def time():
+    string=strftime('%H:%M:%S %p')
+    label.config(text=string)
+    label.after(1000,time)
+
+label = Label(root, font=("Helvetica",50), background="black", foreground="cyan")
+label.pack(anchor="center")
+time()
+
+mainloop()
